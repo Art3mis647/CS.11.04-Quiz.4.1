11bb6ce940e52296d71dc0ecc86438677e239e6e
tonyshu <tonynshu@gmail.com>
2023-11-03 08:23:39 +0800

    finishe??

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,27 @@ public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
+    public static void main(String[] args) {
+        String one = "the-stealth-warrior";
+        String two = "The_Stealth_Warrior";
+        String three = "The_Stealth-Warrior";
+        String four = "-no_shot_bruh_";
+        System.out.println(camelCaseMe(one));
+        System.out.println(camelCaseMe(two));
+        System.out.println(camelCaseMe(three));
+        System.out.println(camelCaseMe(four));
+
+
+    }
+    public static int numberOfVowels(String str){
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
+                count++;
+            }
+        }
+        return count;
+    }
     
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
@@ -14,6 +35,17 @@ public class Main {
      4 - divides integer by 2
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
+    public static int notDivisibleBy235(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
@@ -24,6 +56,25 @@ public class Main {
      "the-stealth-warrior" gets converted to "theStealthWarrior"
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
+    public static String camelCaseMe(String input) {
+        String[] words = input.split("[-_]");
+        StringBuilder result = new StringBuilder();
 
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (i == 0) {
+                result.append(word);
+            } else {
+                result.append(Character.toUpperCase(word.charAt(0)));
+                if (word.length() > 1) {
+                    result.append(word.substring(1).toLowerCase());
+                }
+            }
+        }
 
+        return result.toString();
+    }
 }
+
+
+
